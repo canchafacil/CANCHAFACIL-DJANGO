1c93f24ae883ff4d41e9c87edd74b0f674164c3d
Yojan Castañeda <yojancastaneda15@gmail.com>
2026-08-12 07:27:18 -0500

    arreglo mensajes de error

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,12 @@ DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cancha_facil',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'cancha_facil',
+        #'USER': 'root',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '3306',
     }
 }
 
